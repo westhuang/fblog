47bebd59b49acc77f3680572f00b4092f639bafa
westhuang <375076003@qq.com>
2018-05-08 20:36:24 +0800

    second

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,17 +7,28 @@ Page({
     microBlogs: [{
                   "time":"2018-05-06 12:00:23", 
                   "data":"second blog", 
-                  "pic":"http://pic1.sc.chinaz.com/files/pic/pic9/201805/wpic790.jpg"
+                  "pic":["http://pic1.sc.chinaz.com/files/pic/pic9/201805/wpic790.jpg",
+"http://pic1.sc.chinaz.com/files/pic/pic9/201805/wpic791.jpg"]
                   }, 
                   {
                   "time": "2018-05-06 12:00:23",
                   "data": "second blog",
-                  "pic": "http://pic1.sc.chinaz.com/files/pic/pic9/201805/wpic790.jpg"
+                  "pic": ["http://pic1.sc.chinaz.com/files/pic/pic9/201805/wpic792.jpg",              "http://pic1.sc.chinaz.com/files/pic/pic9/201805/wpic793.jpg",
+"http://pic1.sc.chinaz.com/files/pic/pic9/201805/wpic794.jpg"]
                   }
                 ]
   },
 
   //事件处理函数
   onLoad: function () {
+  },
+
+  previewImage: function (e) {
+    var param = e.target.dataset;
+    
+    wx.previewImage({
+      current: param.src,
+      urls: this.data.microBlogs[param.idx].pic
+    })
   }
 })
